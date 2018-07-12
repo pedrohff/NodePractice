14da6e4ef95dca3849d869260130426ee1250bbe
Pedro <pedrohenriqueff@gmail.com>
2018-07-12 00:29:17 -0300

    Udemy course, secton 11/15

--- vidly/index.js
+++ vidly/index.js
@@ -1,3 +1,6 @@
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
 const config = require('config');
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
@@ -6,10 +9,15 @@ const users = require('./routes/users.js');
 const auth = require('./routes/auth.js');
 const express = require('express');
 const app = express();
+// Middlewares
+const error = require('./middleware/error');
+
+winston.add(winston.transports.File, { filename: 'logfile.log' })
+winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' })
 
 if(!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  //export vidly_jwtPrivateKey=mySecureKey
+  //export vidly_jwtPrivateKey=mySecureKey 
   process.exit(1);
 }
 mongoose.connect('mongodb://localhost/vidly')
@@ -24,6 +32,8 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/users', users)
 app.use('/api/auth', auth)
+
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { Genre, validate } = require("../models/genre");
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 router.get("/", async (req, res) => {
+  throw new Error('Could not get the genres');
   const genres = await Genre.find().sort("name");
   res.send(genres);
 });
@@ -35,9 +37,9 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", async (req, res) => {
-  const genre = await Genre.findByIdAndRemove(req.param.id);
-
+router.delete("/:id", [auth, admin], async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+  
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found.");
 
